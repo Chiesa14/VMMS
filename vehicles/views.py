36bb1b6d2b917e93cc65670a717913b5533ebdc5
Chiesa14 <remychiesa14@gmail.com>
2024-10-18 10:31:55 +0200

    Vehicle app

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import VehicleForm
+from .models import Vehicles
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello World!')
+    vehicles = Vehicles.objects.all()
+    return render(request, 'vehicles.html', {'vehicles': vehicles})
+
+def create_vehicle(request):
+    if request.method == 'POST':
+        form = VehicleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # Redirect to home page after creating vehicle
+    else:  # Handle GET request to render the form
+        form = VehicleForm()
+
+
+    return render(request, 'vehicle_form.html', {'form': form})
